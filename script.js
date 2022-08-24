ca3941624b864731b19a5be02ebb792d8802c403
sabamgeliashvili <sba69717@gmail.com>
2022-08-24 17:09:23 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+// // task 1 ///////
+
+
+// arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// let result = arr1.reduce((acc, cur)=>{
+//      if(acc[cur]){
+//         acc[cur]++
+// }else acc[cur] = 1;
+// return acc
+// console.log(cur,acc)
+// },{})
+// console.log(result)
+
+
+// task 3 ///////
+
+
+
+// let array1 = [1, 2, 3];
+// let array2 = [2, 30, 1];
+
+// const x = Object.assign(array1,array2)
+// console.log(x)
+
+
+
+
+// task 4 ///////
+
+//   const users = [
+//       {
+//           name: "Angelina Jolie",
+//           age: 80
+//       },
+//       {
+//           name: "Eric Jones",
+//           age: 2
+//       },
+//       {
+//           name: "Paris Hilton",
+//           age: 5
+//       },
+//       {
+//           name: "Kayne West",
+//           age: 16
+//       },
+//       {
+//           name: "Bob Ziroll",
+//           age: 100
+//       }
+//   ]; 
+//  const x = users.map((users) => {
+// let fullinfo = "<h1>" + users.name + "</h1>" + "<h2>" + users.age + "</h2>";
+// return fullinfo
+//  });
+//  console.log(x)
+
+
+
+
+
+
+
+
